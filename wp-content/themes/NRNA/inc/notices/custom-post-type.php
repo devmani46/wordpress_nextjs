72f56dbc142ac258b5bc11c2ff68b6f37cab55cd
devmani <devmani1456@gmail.com>
2025-11-10 12:46:50 +0545

    categories added in noices

--- wp-content/themes/NRNA/inc/notices/custom-post-type.php
+++ wp-content/themes/NRNA/inc/notices/custom-post-type.php
@@ -29,6 +29,36 @@ function nrna_register_notice_cpt() {
 }
 add_action('init', 'nrna_register_notice_cpt');
 
+// Register Notice Category taxonomy
+function nrna_register_notice_category_taxonomy() {
+    $labels = [
+        'name'              => _x('Notice Categories', 'taxonomy general name', 'nrna'),
+        'singular_name'     => _x('Notice Category', 'taxonomy singular name', 'nrna'),
+        'search_items'      => __('Search Notice Categories', 'nrna'),
+        'all_items'         => __('All Notice Categories', 'nrna'),
+        'parent_item'       => __('Parent Notice Category', 'nrna'),
+        'parent_item_colon' => __('Parent Notice Category:', 'nrna'),
+        'edit_item'         => __('Edit Notice Category', 'nrna'),
+        'update_item'       => __('Update Notice Category', 'nrna'),
+        'add_new_item'      => __('Add New Notice Category', 'nrna'),
+        'new_item_name'     => __('New Notice Category Name', 'nrna'),
+        'menu_name'         => __('Notice Categories', 'nrna'),
+    ];
+
+    $args = [
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => ['slug' => 'notice-category'],
+        'show_in_rest'      => true,
+    ];
+
+    register_taxonomy('notice_category', ['notices'], $args);
+}
+add_action('init', 'nrna_register_notice_category_taxonomy');
+
 // Register meta fields for REST API
 function nrna_register_notices_meta_rest() {
     register_meta('post', 'notice_content', array(
@@ -56,10 +86,15 @@ function nrna_register_notices_meta_rest() {
 }
 add_action('init', 'nrna_register_notices_meta_rest');
 
-// Prepare REST API response to include meta fields
+// Prepare REST API response to include meta fields and categories
 function nrna_prepare_notices_rest($response, $post, $request) {
     $response->data['notice_content'] = get_post_meta($post->ID, 'notice_content', true);
     $response->data['notice_related'] = get_post_meta($post->ID, 'notice_related', false);
+
+    // Add notice categories
+    $categories = wp_get_post_terms($post->ID, 'notice_category', ['fields' => 'all']);
+    $response->data['notice_categories'] = $categories;
+
     return $response;
 }
 add_filter('rest_prepare_notices', 'nrna_prepare_notices_rest', 10, 3);
