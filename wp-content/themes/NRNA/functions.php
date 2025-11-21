b9bd06f74d205f78c39df48c114660bed9847389
devmani <devmani1456@gmail.com>
2025-11-21 09:56:08 +0545

    cleaning function.php

--- wp-content/themes/NRNA/functions.php
+++ wp-content/themes/NRNA/functions.php
@@ -6,84 +6,8 @@
 // Load includes
 $inc = get_template_directory() . '/inc/';
 
-require_once $inc . 'enqueue-scripts.php';
-require_once $inc . 'menu-api.php';
-
-// Load home meta box
-require_once $inc . 'home-meta-box.php';
-
-// Load who-we-are meta box
-require_once $inc . 'who-we-are-meta-box.php';
-
-// Load organizational structure meta box
-require_once $inc . 'organizational-structure-meta-box.php';
-
-// Load contact meta box
-require_once $inc . 'contact-meta-box.php';
-
-// Load nrna discount meta box
-require_once $inc . 'nrna-discount-meta-box.php';
-
-
-
-// Load projects includes
-require_once $inc . 'projects/custom-post-type.php';
-require_once $inc . 'projects/meta-boxes.php';
-
-// Load events includes
-require_once $inc . 'events/custom-post-type.php';
-require_once $inc . 'events/meta-boxes.php';
-
-
-// Load faqs includes
-require_once $inc . 'faqs/custom-post-type.php';
-require_once $inc . 'faqs/meta-boxes.php';
-
-// Load notices includes
-require_once $inc . 'notices/custom-post-type.php';
-require_once $inc . 'notices/meta-boxes.php';
-
-// Load activities includes
-require_once $inc . 'activities/custom-post-type.php';
-require_once $inc . 'activities/meta-boxes.php';
-
-// Load resources includes
-require_once $inc . 'resources/custom-post-type.php';
-require_once $inc . 'resources/meta-boxes.php';
-
-// Load reports & publications includes
-require_once $inc . 'reports-publications/custom-post-type.php';
-require_once $inc . 'reports-publications/meta-boxes.php';
-
-// Load news includes
-require_once $inc . 'news/custom-post-type.php';
-require_once $inc . 'news/meta-boxes.php';
-
-// Load executive committee includes
-require_once $inc . 'executive-committee/custom-post-type.php';
-require_once $inc . 'executive-committee/meta-boxes.php';
-
-// Load galleries includes
-require_once $inc . 'galleries/custom-post-type.php';
-require_once $inc . 'galleries/meta-boxes.php';
-
-// Load videos includes
-require_once $inc . 'videos/custom-post-type.php';
-require_once $inc . 'videos/meta-boxes.php';
-
-// Load committees, taskforces & subcommittees includes
-require_once $inc . 'committees-taskforces-subcommittees/index.php';
-
-// Load our ncc includes
-require_once $inc . 'our-ncc/index.php';
-
-// Load privacy policy includes
-require_once $inc . 'privacy-policy/custom-post-type.php';
-require_once $inc . 'privacy-policy/meta-boxes.php';
-
-// Load terms and conditions includes
-require_once $inc . 'terms-and-conditions/custom-post-type.php';
-require_once $inc . 'terms-and-conditions/meta-boxes.php';
+// Load all includes
+require_once $inc . 'index.php';
 
 // Theme setup
 function nrna_theme_setup() {
---
+++ wp-content/themes/NRNA/inc/index.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * NRNA Theme Includes
+ */
+
+// Load enqueue meta box
+require_once 'enqueue-scripts.php';
+
+// Load menu-api meta box
+require_once 'menu-api.php';
+
+// Load home meta box
+require_once 'home-meta-box.php';
+
+// Load who-we-are meta box
+require_once 'who-we-are-meta-box.php';
+
+// Load organizational structure meta box
+require_once 'organizational-structure-meta-box.php';
+
+// Load contact meta box
+require_once 'contact-meta-box.php';
+
+// Load nrna discount meta box
+require_once 'nrna-discount-meta-box.php';
+
+// Load projects includes
+require_once 'projects/index.php';
+
+// Load events includes
+require_once 'events/index.php';
+
+// Load faqs includes
+require_once 'faqs/index.php';
+
+// Load notices includes
+require_once 'notices/index.php';
+
+// Load activities includes
+require_once 'activities/index.php';
+
+// Load resources includes
+require_once 'resources/index.php';
+
+// Load reports & publications includes
+require_once 'reports-publications/index.php';
+
+// Load news includes
+require_once 'news/index.php';
+
+// Load executive committee includes
+require_once 'executive-committee/index.php';
+
+// Load galleries includes
+require_once 'galleries/index.php';
+
+// Load videos includes
+require_once 'videos/index.php';
+
+// Load committees, taskforces & subcommittees includes
+require_once 'committees-taskforces-subcommittees/index.php';
+
+// Load our ncc includes
+require_once 'our-ncc/index.php';
+
+// Load privacy policy includes
+require_once 'privacy-policy/index.php';
+
+// Load terms and conditions includes
+require_once 'terms-and-conditions/index.php';
