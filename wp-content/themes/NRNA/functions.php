33738a5da516acf5cad15901038047481c94350b
devmani <devmani1456@gmail.com>
2025-12-19 11:01:53 +0545

    cleaning functions

--- wp-content/themes/NRNA/functions.php
+++ wp-content/themes/NRNA/functions.php
@@ -23,32 +23,3 @@ add_action('after_setup_theme', 'nrna_theme_setup');
 
 // Load Parent Menus
 require_once $inc . 'parent-menus.php';
-
-
-// Load Contact Us
-require_once $inc . 'contact-us.php';
-
-// Load Subscribe
-require_once $inc . 'subscribe.php';
-
-// Load Event Registrations
-require_once $inc . 'event-registrations.php';
-
-// Load Regional Meeting Registrations
-require_once $inc . 'regional-meeting-registrations.php';
-
-// Load Search API
-require_once $inc . 'search-api.php';
-
-// Load Project Inquiries
-require_once $inc . 'project-inquiries.php';
-
-// Auto-update slug when title updates
-function nrna_auto_update_slug($data, $postarr)
-{
-    if (!in_array($data['post_status'], ['draft', 'pending', 'auto-draft'])) {
-        $data['post_name'] = sanitize_title($data['post_title']);
-    }
-    return $data;
-}
-add_filter('wp_insert_post_data', 'nrna_auto_update_slug', 99, 2);
--- wp-content/themes/NRNA/inc/index.php
+++ wp-content/themes/NRNA/inc/index.php
@@ -78,3 +78,33 @@ require_once 'privacy-policy-meta-box.php';
 
 // Load terms and conditions meta box
 require_once 'terms-and-conditions-meta-box.php';
+
+// Load forms
+// Load Contact Us
+require_once $inc . 'contact-us.php';
+
+// Load Subscribe
+require_once $inc . 'subscribe.php';
+
+// Load Event Registrations
+require_once $inc . 'event-registrations.php';
+
+// Load Regional Meeting Registrations
+require_once $inc . 'regional-meeting-registrations.php';
+
+// Load Search API
+require_once $inc . 'search-api.php';
+
+// Load Project Inquiries
+require_once $inc . 'project-inquiries.php';
+
+
+// Auto-update slug when title updates
+function nrna_auto_update_slug($data, $postarr)
+{
+    if (!in_array($data['post_status'], ['draft', 'pending', 'auto-draft'])) {
+        $data['post_name'] = sanitize_title($data['post_title']);
+    }
+    return $data;
+}
+add_filter('wp_insert_post_data', 'nrna_auto_update_slug', 99, 2);
